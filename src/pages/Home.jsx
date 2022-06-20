87d198360b59000f6c50c78ad9d1c8b37d4c4763
mustapha335 <reezy77jay@gmail.com>
2022-06-20 12:27:58 +0100

    products completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Slider />
+      <Categories />
+      <Products />
     </div>
   );
 };
