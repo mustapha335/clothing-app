46e0346ebd0d1ca57658c3e757176f5dc6c7e02b
mustapha335 <reezy77jay@gmail.com>
2022-06-21 15:09:21 +0100

    productPage completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Product from "./pages/Product";
 // import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
+// import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className='App'>
-      <ProductList />
+      <Product />
     </div>
   );
 }
